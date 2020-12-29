ad642aefd28bc86b90666d69adc72edea622224e
lukaszszymankiewicz <lukasz_szymankiewicz@op.pl>
2020-12-29 13:03:23 +0100

    transformations of results added

--- main.py
+++ main.py
@@ -33,18 +33,90 @@ def place_queens(rows: set) -> bool:
     return True
 
 
-a = set(range(size))
-
 results = []
 
 coords_set = {
-    (row, col): calculate_coords(row, col)
-    for row in range(0, size)
-    for col in range(0, size)
+    (row, col): calculate_coords(row, col) for row in range(0, size) for col in range(0, size)
 }
 
-for rows_combination in permutations(a):
+# ALL POSSIBLE COMBINATIONS
+for rows_combination in permutations(range(size)):
     result = place_queens(rows_combination)
 
     if result:
-        results.append(rows_combination)
+        results.append(list(rows_combination))
+
+# TRANSFORMATIONS:
+def vertical_symmetry(combination):
+    return combination[::-1]
+
+
+def horizontal_symmetry(combination):
+    return [size - number - 1 for number in combination]
+
+
+def rot90(combination):
+    numbers = range(8)
+    result = [0] * 8
+    orders = [7 - c for c in combination]
+
+    for number, order in zip(numbers, orders):
+        result[order] = number
+    return result
+
+
+def rot180(combination):
+    return [7 - c for c in combination][::-1]
+
+
+def rot270(combination):
+    numbers = range(8)[::-1]
+    result = [0] * 8
+
+    for number, order in zip(numbers, combination):
+        result[order] = number
+
+    return result
+
+
+def diag_symmetry(combination):
+    numbers = range(8)
+    result = [0] * 8
+
+    for number, order in zip(numbers, combination):
+        result[order] = number
+
+    return result
+
+
+def antidiag_symmetry(combination):
+    numbers = range(8)[::-1]
+    orders = [7 - c for c in combination]
+    result = [0] * 8
+
+    for number, order in zip(numbers, orders):
+        result[order] = number
+
+    return result
+
+
+# DELETING REDUNDANT COMBINATIONS
+transformations = [
+    vertical_symmetry,
+    horizontal_symmetry,
+    rot90,
+    rot180,
+    rot270,
+    antidiag_symmetry,
+    diag_symmetry,
+]
+
+for combination in results:
+    for transformation in transformations:
+        transformed_result = transformation(combination)
+
+        if transformed_result in results:
+            results.remove(transformed_result)
+
+for result in results:
+    print(result)
