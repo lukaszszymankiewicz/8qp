017c243d30fb92b3f71730571ff09e1d15e1c004
lukaszszymankiewicz <lukasz_szymankiewicz@op.pl>
2021-01-22 13:07:59 +0100

    memory freeing in C added

--- app/src/alg.c
+++ app/src/alg.c
@@ -1,37 +1,27 @@
 #include <stdio.h>
+#include <string.h>
 
 #define DIAG(r, c) (r+c)
 #define ANTIDIAG(r, c) (8+c-r-1)
 
+int *solve();
 int combination_is_solution(int combination[]);
 void swap(int *a, int *b);
-void check_hit(int r, int c, char arr[8]);
 void permutation(int *arr, int start, int end);
-int *solve();
 
-int results[92][8];
-int counter = 0;
-
-int main()
-{
-    solve();
-    int i;
-    for (i=0; i<8; i++)
-    {
-        printf("%d", results[1][i]);
-    }
-    return 0;
-}
+int results[92][8] = { 0 };
+int counter;
 
 int *solve()
 {
+    memset(results, 0, sizeof(results[0][0]) * 8 * 92);
+    counter=0;
     int size=7;
     int arr[8] = {0, 1, 2, 3, 4, 5, 6, 7};
     permutation(arr, 0, size);
 
     return *results;
 }
-
 
 int combination_is_solution(int combination[8]) 
 {
